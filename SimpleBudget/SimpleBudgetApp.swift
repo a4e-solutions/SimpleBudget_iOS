174626b0a341b14bb8ee1277709e59294b32087a
andre fourie <afourie.nz@gmail.com>
2024-10-29 15:13:09 +1300

    Initial Commit

---
+++ SimpleBudget/SimpleBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBudgetApp.swift
+//  SimpleBudget
+//
+//  Created by andre fourie on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
